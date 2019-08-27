5aca5bed25335205e1c66431d4d35c009b905e86
Saeed Altaf <saeedaltaf@gmail.com>
2019-08-27 12:33:40 -0400

    Created home page html

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+//Array of friends data:
+
+var friendsData = [{
+        name: "Jane",
+        photo: "jane.jpeg",
+        scores: [
+            "4",
+            "1",
+            "2",
+            "4",
+            "5",
+            "5",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Rafael",
+        photo: "rafael.jpeg",
+        scores: [
+            "3",
+            "1",
+            "5",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Xiomara",
+        photo: "xiomara.jpeg",
+        scores: [
+            "4",
+            "5",
+            "3",
+            "2",
+            "5",
+            "4",
+            "2",
+            "5",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Rogelio",
+        photo: "rogelio.jpeg",
+        scores: [
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "4",
+            "3"
+        ]
+    }
+];
+
+module.exports = friendsData;
